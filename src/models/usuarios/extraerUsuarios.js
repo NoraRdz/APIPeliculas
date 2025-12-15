b10e8204d2e88c7f862aaf27100778b94d428b9d
NoraRdz <narl031003@gmail.com>
2025-12-14 22:23:02 -0600

    se agregaron ejemplos de codigo

--- src/models/usuarios/extraerUsuarios.js
+++ src/models/usuarios/extraerUsuarios.js
@@ -4,7 +4,7 @@ import {pool} from "../../config/postgre.js";
  * Actualiza la información de un usuario existente.
  * Utiliza COALESCE en SQL para mantener los valores actuales si los nuevos son nulos.
  *
- * @module Models/usuarios/actualizarUsuario
+ * @module Models/usuarios/extraerUsuarios
  *
  * @param {Object} usuario - Objeto con los datos a actualizar.
  * @param {number} usuario.id - ID del usuario a modificar.
@@ -14,28 +14,18 @@ import {pool} from "../../config/postgre.js";
  * @returns {Promise<Array<Object>|string>} Retorna el registro actualizado.
  * Si ocurre un error, retorna "Error en el servidor".
  */
-export default async function actualizarUsuario(usuario) {
+export default async function extraerUsuarios() {
   try {
     const result = await pool.query(
       `
-        UPDATE users 
-        SET 
-            email    = COALESCE($1, email),
-            password = COALESCE($2, password)
-        WHERE id = $3
-        RETURNING *
-      `,
-      [
-        usuario.email || null,
-        usuario.password || null,
-        usuario.id
-      ]
+        select * from users
+      `
     );
 
     console.table(result.rows);
     return result.rows;
   } catch (err) {
     console.error(err);
-    return "Error en el servidor";
+    throw "Error en el servidor";
   }
 }
